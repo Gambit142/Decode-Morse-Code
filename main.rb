57aa67ed94d3602f8f37fe9bd2edbefb92b7deaa
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-12-09 13:00:21 +0100

    Add code to decode morse words and letters

--- main.rb
+++ main.rb
@@ -27,3 +27,16 @@ $morse_object = {
 "--.." => "Z",
 "/" => " ",
 }
+
+def decode_char(a)
+  return $morse_object[a].upcase()
+end 
+
+puts decode_char('.-')
+
+def decode_word(a)
+  result = ''
+  split_word = a.split(' ')
+  split_word.each do |word| result += decode_char(word) end
+  return result.upcase()
+end
