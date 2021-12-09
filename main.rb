9414479876b0f3d962575b1d70686d8cd11a15d9
Busiwa24 <busiwaliuma@gmail.com>
2021-12-09 14:59:34 +0200

    added final morse functions

--- main.rb
+++ main.rb
@@ -40,3 +40,14 @@ def decode_word(a)
   split_word.each do |word| result += decode_char(word) end
   return result.upcase()
 end
+
+puts decode_word("-- .- -.--")
+
+def decode_morse_code(a)
+  morse_words = []
+  split_word = a.split('   ')
+  split_word.each do |word| morse_words.push(decode_word(word)) end
+  return morse_words.join(' ').upcase()
+end
+
+puts decode_morse_code(' .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
