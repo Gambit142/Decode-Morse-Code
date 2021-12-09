b012f4e4138051fa0aab6e3a4999fd37694c5881
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-12-09 14:36:38 +0100

    Install gem files and correct all linters errors

--- main.rb
+++ main.rb
@@ -1,53 +1,33 @@
-$morse_object = { 
-".-" => "A",
-"-..." => "B",
-"-.-." => "C",
-"-.." => "D",
-"." => "E",
-"..-." => "F",
-"--." => "G",
-"...." => "H",
-".."=> "I",
-".---" => "J",
-"-.-" => "K",
-".-.." => "L",
-"--" => "M",
-"-." => "N",
-"---" => "O",
-".---." => "P",
-"--.-" => "Q",
-".-." => "R",
-"..." => "S",
-"-" => "T",
-"..-" => "U",
-"...-" => "V",
-".--" => "W",
-"-..-" => "X",
-"-.--" => "Y",
-"--.." => "Z",
-"/" => " ",
-}
+def decode_char(code)
+  morse_object = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.---.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z', '/' => ' '
+  }
 
-def decode_char(a)
-  return $morse_object[a].upcase()
-end 
+  morse_object[code].upcase
+end
 
 puts decode_char('.-')
 
-def decode_word(a)
+def decode_word(code)
   result = ''
-  split_word = a.split(' ')
-  split_word.each do |word| result += decode_char(word) end
-  return result.upcase()
+  split_word = code.split
+  split_word.each { |word| result += decode_char(word) }
+  result.upcase
 end
 
-puts decode_word("-- .- -.--")
+puts decode_word('-- .- -.--')
 
-def decode_morse_code(a)
+def decode_morse_code(code)
   morse_words = []
-  split_word = a.split('   ')
-  split_word.each do |word| morse_words.push(decode_word(word)) end
-  return morse_words.join(' ').upcase()
+  split_word = code.split('   ')
+  split_word.each { |word| morse_words.push(decode_word(word)) }
+  morse_words.join(' ').upcase
 end
 
 puts decode_morse_code(' .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
